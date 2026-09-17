b3a217bea18baeef1706f7ed88215eb7021c55dc
David <davmarrod3@alu.edu.gva.es>
2026-09-17 18:36:38 +0200

    Guardado

--- proyectoVanilla/src/UT02/Ejercicio 1/main.js
+++ proyectoVanilla/src/UT02/Ejercicio 1/main.js
@@ -4,8 +4,8 @@ console.log(`El imc es ${IMC(70, 1.75)}`);
 
 let imcJuan = IMC(70, 1.75);
 let imcMarcos = IMC(75, 1.75);
-let tieneImcMayor = (IMC(70, 1.75) == IMC(75, 1.75));
+let tieneImcMayor = (imcJuan > imcMarcos);
+
 console.log(`El imc de Juan es, ${imcJuan} y el imc de Marcos es ${imcMarcos} y ${tieneImcMayor}`);
 
 console.log(`Hola mundo, hasta aqui funciona el codigo`);
-
