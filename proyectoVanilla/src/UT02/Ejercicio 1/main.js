45dfb99e3599101072e60068551c02ccc4147725
David <davmarrod3@alu.edu.gva.es>
2026-09-21 19:08:40 +0200

    Actualizado 21/09

--- proyectoVanilla/src/UT02/Ejercicio 1/main.js
+++ proyectoVanilla/src/UT02/Ejercicio 1/main.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import { imc, imprimirDuplicados} from "../bibliotecas/imports.js";
+=======
+import { imc, imprimirDuplicados, calcularFacturas} from "../bibliotecas/imports.js";
+>>>>>>> da9ca14 (Actualizado 21/09)
 
 const imcJuan = imc(70, 1.75); //masa, altura
 const imcMarcos = imc(75, 1.75);
@@ -9,4 +13,9 @@ console.log(`¿Tiene Marcos un IMC mayor que el de Juan?: ${marcosTieneMayorImc}
 
 //ejercicio 3
 
+<<<<<<< HEAD
 imprimirDuplicados(4,6);
+=======
+imprimirDuplicados(4,6);
+calcularFacturas(`a`,`a`);
+>>>>>>> da9ca14 (Actualizado 21/09)
--- proyectoVanilla/src/UT02/bibliotecas/imports.js
+++ proyectoVanilla/src/UT02/bibliotecas/imports.js
@@ -4,13 +4,4 @@ function imc(masa, altura){
     return masa / (altura * altura);
 }
 
-function imprimirDuplicados(pasadas,numeroInicial){
-        let contador = 1;
-    for (let i = 0; i < pasadas; i++) {
-        console.log(`Pasada: ${contador} ${numeroInicial}`);
-        numeroInicial = numeroInicial *2;
-        contador = contador + 1;
-    }
-}
-
-export { imc, imprimirDuplicados};
+export { IMC };
