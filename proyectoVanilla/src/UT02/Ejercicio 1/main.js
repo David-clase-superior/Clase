ea6811276c8ef889a5a6676d9cd10f60e22a455f
David <davmarrod3@alu.edu.gva.es>
2026-09-23 15:47:31 +0200

    23/9

--- proyectoVanilla/src/UT02/Ejercicio 1/main.js
+++ proyectoVanilla/src/UT02/Ejercicio 1/main.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import { imc, imprimirDuplicados} from "../bibliotecas/imports.js";
-=======
-import { imc, imprimirDuplicados, calcularFacturas} from "../bibliotecas/imports.js";
->>>>>>> da9ca14 (Actualizado 21/09)
-
 const imcJuan = imc(70, 1.75); //masa, altura
 const imcMarcos = imc(75, 1.75);
 const marcosTieneMayorImc = imcMarcos > imcJuan;
@@ -12,10 +6,3 @@ console.log(`IMC de Marcos ${imcMarcos}`);
 console.log(`¿Tiene Marcos un IMC mayor que el de Juan?: ${marcosTieneMayorImc}`);
 
 //ejercicio 3
-
-<<<<<<< HEAD
-imprimirDuplicados(4,6);
-=======
-imprimirDuplicados(4,6);
-calcularFacturas(`a`,`a`);
->>>>>>> da9ca14 (Actualizado 21/09)
