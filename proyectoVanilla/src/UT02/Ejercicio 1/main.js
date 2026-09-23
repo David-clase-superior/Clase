4c61b152792ad18001042ae2da656943dc6cbd07
David <davmarrod3@alu.edu.gva.es>
2026-09-23 15:49:35 +0200

    23/9_2

--- proyectoVanilla/src/UT02/Ejercicio 1/main.js
+++ proyectoVanilla/src/UT02/Ejercicio 1/main.js
@@ -1,3 +1,5 @@
+import { imc, imprimirDuplicados, calcularFacturas} from "../bibliotecas/imports.js";
+
 const imcJuan = imc(70, 1.75); //masa, altura
 const imcMarcos = imc(75, 1.75);
 const marcosTieneMayorImc = imcMarcos > imcJuan;
@@ -6,3 +8,6 @@ console.log(`IMC de Marcos ${imcMarcos}`);
 console.log(`¿Tiene Marcos un IMC mayor que el de Juan?: ${marcosTieneMayorImc}`);
 
 //ejercicio 3
+
+imprimirDuplicados(4,6);
+calcularFacturas(`a`,`a`);
--- proyectoVanilla/src/UT02/bibliotecas/imports.js
+++ proyectoVanilla/src/UT02/bibliotecas/imports.js
@@ -4,4 +4,26 @@ function imc(masa, altura){
     return masa / (altura * altura);
 }
 
-export { IMC };
+function imprimirDuplicados(pasadas,numeroInicial){
+        let contador = 1;
+    for (let i = 0; i < pasadas; i++) {
+        console.log(`Pasada: ${contador} ${numeroInicial}`);
+        numeroInicial = numeroInicial *2;
+        contador = contador + 1;
+    }
+}
+
+function calcularFacturas(nombreProducto= `Producto_Generico`, precioProducto = 100, impuestoProducto = 21){
+    let validacion = true;
+    while(validacion === false){
+        
+    if(precioProducto !== isNaN || impuestoProducto !== isNaN){
+        console.log(`Error. Número no válido`);
+    }
+    continue;
+    }
+
+    let precioCalculado = (precioProducto * 100) / impuestoProducto;
+    console.log(`${nombreProducto}, ${precioCalculado}`);
+}
+export { imc, imprimirDuplicados, calcularFacturas};
